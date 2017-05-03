9181c8304158a76fa4e0701fdb2ab605b645eeda
noi narisak <noi.narisak@gmail.com>
2017-05-03 09:46:41 -0500

    Add my update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-ANDROID_SDK_FILENAME=android-sdk_r24.2-linux.tgz
+ANDROID_SDK_FILENAME=android-sdk_r24.4.1-linux.tgz
 ANDROID_SDK=http://dl.google.com/android/$ANDROID_SDK_FILENAME
 
 #sudo apt-get install python-software-properties
 #sudo add-apt-repository ppa:webupd8team/java
 apt-get update
-apt-get install -y npm git openjdk-7-jdk ant expect
+apt-get install -y npm git openjdk-7-jdk ant expect 
+apt-get install -y lib32stdc++6 lib32z1
 npm install -g n
 n stable
 
@@ -15,18 +16,21 @@ tar -xzvf $ANDROID_SDK_FILENAME
 sudo chown -R vagrant android-sdk-linux/
 
 echo "ANDROID_HOME=~/android-sdk-linux" >> /home/vagrant/.bashrc
-echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386" >> /home/vagrant/.bashrc
+# echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386" >> /home/vagrant/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /home/vagrant/.bashrc
 echo "PATH=\$PATH:~/android-sdk-linux/tools:~/android-sdk-linux/platform-tools" >> /home/vagrant/.bashrc
 
-npm install -g cordova
-npm install -g ionic
 expect -c '
 set timeout -1   ;
-spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-22,build-tools-22.0.1
+spawn /home/vagrant/android-sdk-linux/tools/android update sdk -u --all --filter platform-tool,android-23,build-tools-23.0.3
 expect { 
     "Do you accept the license" { exp_send "y\r" ; exp_continue }
     eof
 }
 '
+
+# Ionic CLI tools
+sudo npm install -g cordova
+sudo npm install -g ionic
 
 #sudo gem install sass
